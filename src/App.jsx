e4512bf354c0aedf9fde8aa0a92131ac2623bf8e
Sumit-Dhimal <sumit.dhimal.333@gmail.com>
2024-06-24 08:34:50 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import QuoteGenerator from "./QuoteGenerator"
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen h-full bg-purple-800">
+      <div>
+        <QuoteGenerator />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/QuoteGenerator.jsx
@@ -0,0 +1,10 @@
+
+export default function QuoteGenerator() {
+
+    return (
+        <div>
+            
+            <h1>Generate quote</h1>
+        </div>
+    )
+}
